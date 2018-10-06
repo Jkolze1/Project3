6c064a432e32f84f5e7d48c94c321a2e5a15f180
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-10-06 12:45:51 -0500

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -65,4 +65,17 @@ router.post('/logout', (req, res) => {
     }
 })
 
+// Facebook 10/06
+// FACEBOOK ROUTES =====================
+	// =====================================
+	// route for facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
 module.exports = router
