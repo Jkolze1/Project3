8b9b5a2ba02f8220e6d11b40fa9521a6ca11cc53
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-09 04:48:34 -0500

    react boilerplate

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class EventsModal extends Component {
+export default class SignIn extends Component {
   render() {
     return <div />;
   }
---
+++ src/components/favoriteEvents.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class FavoriteEvents extends Component {
+  render() {
+    return <div />;
+  }
+}
