4dc454c269edb86eb79663e220b20a7e3c6a7f6b
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-09 03:51:41 -0500

    components files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import { Route, Link } from 'react-router-dom'
 // components
-import Signup from './components/sign-up'
+import Signup from './components/Sign-Up'
 import LoginForm from './components/login-form'
 import Navbar from './components/navbar'
-import Home from './components/home'
+import Home from './components/Home'
 
 class App extends Component {
   constructor() {
