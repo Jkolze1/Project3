3eb74e976bfc913b86e1057df465fc28bf4be8ee
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-09 15:48:00 -0500

    components

--- src/components/favoritePlaces.js
+++ src/components/favoritePlaces.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-export default class FavoritePlaces extends Component {
+class FavoritePlaces extends Component {
   render() {
     return <div />;
   }
 }
+
+export default FavoritePlaces;
--- src/components/meetUpModal.js
+++ src/components/meetUpModal.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-export default class MeetUpModal extends Component {
+class MeetUpModal extends Component {
   render() {
     return <div />;
   }
 }
+
+export default MeetUpModal;
