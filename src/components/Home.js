f45b22306c41260e75fc8894fee97cf6440d81c1
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-15 16:46:06 -0500

    routing and layout updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Button } from 'reactstrap';
+import { Container, Button, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FavoriteEvents from './FavoriteEvents';
@@ -19,26 +19,30 @@ class Home extends Component {
     return (
       <Router>
         <Container>
-          <Link to="/places/saved">
-            <Button className="mr-2">
-              <i class="fas fa-heart" />
-              See Saved Places
-            </Button>
-          </Link>
-
-          <Link to="/meetup/saved">
-            <Button className="mr-2">
-              <i class="fas fa-heart" />
-              See Saved Groups
-            </Button>
-          </Link>
-
-          <Link to="/events/saved">
-            <Button>
-              <i class="fas fa-heart" />
-              See Saved Events
-            </Button>
-          </Link>
+          <Col>
+            <Link to="/places/saved">
+              <Button className="m-2 btn-lg">
+                <i class="fas fa-heart" />
+                See Saved Places
+              </Button>
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/meetup/saved">
+              <Button className="m-2 btn-lg">
+                <i class="fas fa-heart" />
+                See Saved Groups
+              </Button>
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/events/saved">
+              <Button className="m-2 btn-lg">
+                <i class="fas fa-heart" />
+                See Saved Events
+              </Button>
+            </Link>
+          </Col>
           <Route exact path="/events/saved" component={FavoriteEvents} />
           <Route exact path="/places/saved" component={FavoritePlaces} />
           <Route exact path="/meetup/saved" component={FavoriteMeetUp} />
