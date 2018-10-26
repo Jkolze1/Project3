d71997d2457369c5f9f077fe15b3d894f6df3266
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-26 14:21:30 -0500

    minor changes

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Container,FormGroup, Label, Col, Input, Button } from 'reactstrap';
+import {
+  Container,
+  FormGroup,
+  Label,
+  Col,
+  Row,
+  Input,
+  Button
+} from 'reactstrap';
 
 import { Link } from 'react-router-dom';
 import FavoriteEvents from './FavoriteEvents';
@@ -12,15 +20,17 @@ export default () => {
       <Container className="formpadding">
         <h2>Find Fitness Events!</h2>
         <h5>Search for events in your area</h5>
-        
-        <Col md={2}>
-            <FormGroup>
-              <Label for="exampleZip">Zip</Label>
-              <Input type="text" name="zip" id="exampleZip"/>
-            </FormGroup>  
-          </Col>
 
-          <Link to="/events/results">
+        <FormGroup className="ml-2">
+          <Row>
+            <Col className="col-md-3">
+              <Label for="exampleZip">Zip</Label>
+              <Input type="text" name="zip" id="exampleZip" />
+            </Col>
+          </Row>
+        </FormGroup>
+
+        <Link to="/events/results">
           <Button className="btn btn-primary mt-2 ml-2" type="submit">
             Go!
           </Button>
